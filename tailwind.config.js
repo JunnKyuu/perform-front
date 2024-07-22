468cc78a4265de69dbc79438919bd21b4cf84ea5
JunnKyuu <junnkyuu22@gmail.com>
2024-07-23 00:35:43 +0900

    개발 초기 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // 이 경로에 있는 파일들을 모두 포함
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
